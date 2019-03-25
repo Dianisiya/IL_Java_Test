d6da00877abd907e6e696e5dbf7af9ca94450978
Dianisiya <dianisiya.kolisnichenko@gmail.com>
2019-03-25 11:55:06 +0200

    Knowledge implementation

--- src/person/consciousness/Knowledge.java
+++ src/person/consciousness/Knowledge.java
@@ -1,7 +1,14 @@
 package person.consciousness;
 
 public class Knowledge {
+
+    private int level;
+
     public Knowledge(int level) {
-        //TODO: Implementation is needed
+        this.level = level < 0 ? 0 : level;
+    }
+
+    public boolean isHigherThat (double requiredLevel) {
+        return Double.compare(this.level, requiredLevel) > 0;
     }
 }
